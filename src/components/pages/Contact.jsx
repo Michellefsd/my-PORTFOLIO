19c5bb07cee140c4290fb22ba79126ebf955adc8
Michellefsd <promichfsd@gmail.com>
2023-05-05 16:55:28 -0300

    c

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,25 +1,31 @@
 import '../Button.css';
 import './Contact.css';
+import {useState} from 'react';
+import parsePhoneNumber from 'libphonenumber-js';
 import {HiCheckCircle, HiExclamationCircle} from 'react-icons/hi';
 import {GoAlert} from 'react-icons/go';
 import Alert from './Alert';
-import {useState} from 'react';
 
 function Contact() {
   const style = {color: 'var(--neutral_color--80)', fontSize: '12px'} 
-  const [emailValue, setEmailValue] = useState("");
   const [nameValue, setNameValue] = useState("");
+  const [nameState, setNameState] = useState(null);
+
+  const [emailValue, setEmailValue] = useState("");
+  const [emailState, setEmailState] = useState(null);
+  const validator = require("email-validator");
+
   const [phoneValue, setPhoneValue] = useState("");
+  const [phoneState, setPhoneState] = useState(null);
 
   ///////////////////////////////////////
   // usar useEffect xa q renderize al momento
   const [messageValue, setMessageValue] = useState("");
+  const [count, setCount] = useState(250)
 
 
-  const [state, setState] = useState(null);
   const [children, setChildren] =useState(null)
   const [icon, setIcon] = useState(null);
-  const [count, setCount] = useState(250)
 
   const handleNameChange = (e) => {
     setNameValue(e.target.value);
@@ -42,17 +48,33 @@ function Contact() {
   };
   const nonValidateInput = (e) => {
     return (
-      <Alert icon={<GoAlert />}> `Enter a valid ${e.id}`</Alert>
+      <Alert icon={<GoAlert />}> `Enter a valid ${e.target.id}`</Alert>
     )
   };
+  const validateFunctions = (e) => {
+    if(nameValue !== "" ) {
+      setNameState(true);
+      return validateInput;
+    }
+    if(validator.validate(emailValue)){
+      setEmailState(true);
+      return validateInput;
+    }else {
+      setEmailState(false);
+      return nonValidateInput;
+    }
+    if(parsePhoneNumber(phoneValue)){
+      setPhoneState(true);
+      return validateInput;
+    }else {
+      setPhoneState(false);
+      return nonValidateInput;
+    }
+  }
 
   function handleSubmit(e) {
     e.preventDefault();
-    if(nameValue !== "" ) {
-      setState(true);
-      return state && validateInput;
-    }
-  console.log(e.target);
+    
   };
   return (
     <div className='section--contact__div'>
@@ -60,20 +82,20 @@ function Contact() {
         <h2 className='section--title'><span className='underlined'></span>Contact</h2>
         <form onSubmit={handleSubmit} className='form'>
           <label htmlFor="name">Name</label>
-          <input onChange={handleNameChange || ""} value="" className='input' id="name" type="text" />
-          <Alert state={state} icon={icon} style={style}></Alert>
+          <input onChange={handleNameChange || ""} onBlur={validateFunctions} value={nameValue || ""} className='input' id="name" type="text" />
+          <Alert state={nameState} icon={icon} style={style}></Alert>
 
           <label htmlFor="mail">Email</label>
-          <input value={emailValue || ""} onChange={handleMailChange || ""} className='input' id="mail" type="email" />
-          <Alert state={state} icon={icon} style={style}></Alert>
+          <input value={emailValue || ""} onChange={handleMailChange || ""} onBlur={validateFunctions} className='input' id="mail" type="email" />
+          <Alert state={emailState} icon={icon} style={style}></Alert>
 
           <label htmlFor="phone">Phone</label>
           <input value={phoneValue || ""} onChange={handlePhoneChange || ""} className='input' id="phone" type="number" />
-          <Alert state={state} icon={icon} style={style}></Alert>
+          <Alert state={phoneState} icon={icon} style={style}></Alert>
 
           <label htmlFor="message">Message</label>
           <textarea onChange={handleMessageChange|| ""} value={messageValue || ""} id="message" cols="30" rows="10"></textarea>
-          <Alert state={state} icon={icon} style={style}>{count} characters left</Alert>
+          <Alert icon={icon} style={style}>{count} characters left</Alert>
           <button className='btn--orange btn--big mt'>Send</button>
         </form>
       </section>
