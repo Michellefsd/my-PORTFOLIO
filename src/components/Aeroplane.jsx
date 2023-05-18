f85dd362e47b77f5e9e0d68f3487283885e1885f
Michellefsd <promichfsd@gmail.com>
2023-05-18 01:20:15 -0300

    improving animation aeroplane mobile

--- src/components/Aeroplane.jsx
+++ src/components/Aeroplane.jsx
@@ -1,10 +1,16 @@
 import './Aeroplane.css';
 import imgAeroplane from '../img/avion1.png';
+import imgMiniAeroplane from '../img/avion.png';
 
 function Aeroplane() {
   return (
-    <div className='aeroplane-div' > 
-        <img className='aeroplane' src={imgAeroplane} alt="aeroplane" />
+    <div>
+      <div className='mini-aeroplane-div'>
+        <img className='mini-aeroplane' src={imgMiniAeroplane} alt="aeroplane" />
+      </div>
+      <div className='aeroplane-div'> 
+          <img className='aeroplane' src={imgAeroplane} alt="aeroplane" />
+      </div>
     </div>
   );
 }
