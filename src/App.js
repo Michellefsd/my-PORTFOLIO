ee70a13a265f94b927b9d1463c49011d345de836
Michellefsd <promichfsd@gmail.com>
2023-05-21 23:36:01 -0300

    some improvement on modal design

--- src/App.js
+++ src/App.js
@@ -38,13 +38,42 @@ function App() {
       <main className='bk-main'>
         <AnimationSky />
         <Route path="/project-1">
-          <ProjectPage title={"Tocata & Fuga"} collaborators={[{name: "Michelle Rodríguez", as: "Front-end Development", image: {img}}]}img={img1} href={"https://tocata-y-fuga.netlify.app/"} resume={"Simple real project with the intention of spread, promulgate, dates of shows of the Humor and Music humor Company: Tocata & Fuga"} />
+          <ProjectPage 
+            title={"Recreation of e-shop cart"} 
+            img={img1} 
+            resume={"Simple real project with the intention of spread, promulgate, dates of shows of the Humor and Music humor Company: Tocata & Fuga"}
+            href={"https://tocata-y-fuga.netlify.app/"} 
+            collaborators={[{name: "Michelle Rodríguez", as: "Front-end Development", image: {img}}]}
+            list={["React", "CSS", "Github", "Figma", "Node", "Javascript", "Responsive Design", "Validation Form", "Investigation", "Clean Code"]} 
+          />
         </Route>
         <Route path="/project-2">
-          <ProjectPage title={"tarara"} img={img2} resume={"bla bla bla"} />
+          <ProjectPage 
+            title={"Rick & Morty"} 
+            img={img2} 
+            resume={"bla bla bla"} 
+            href={"https://tocata-y-fuga.netlify.app/"} 
+            collaborators={[{name: "Michelle Rodríguez", as: "Front-end Development", image: {img}}]} 
+            list={["React", "CSS", "Github", "Figma", "Node", "Javascript", "Responsive Design", "Validation Form", "Investigation", "Clean Code"]} />
         </Route>
         <Route path="/project-3">
-          <ProjectPage title={"tarara"} img={img3} resume={"bla bla bla"} />
+          <ProjectPage 
+            title={"Tocata & Fuga"} 
+            img={img3} 
+            resume={"Simple real project with the intention of spread, promulgate, dates of shows of the Humor and Music humor Company: Tocata & Fuga"} 
+            href={"https://tocata-y-fuga.netlify.app/"} 
+            collaborators={[{name: "Michelle Rodríguez", as: "Front-end Development", image: {img}}]} 
+            list={["React", "CSS", "Github", "Figma", "Node", "Javascript", "Responsive Design", "Validation Form", "Investigation", "Clean Code"]}/>
+        </Route>
+        <Route path="/project-4">
+          <ProjectPage 
+            title={"Portfolio"} 
+            img={img3} 
+            resume={"bla bla bla"} 
+            href={"https://tocata-y-fuga.netlify.app/"} 
+            collaborators={[{name: "Michelle Rodríguez", as: "Front-end Development", image: {img}}, {name: "Luis Ramírez", as: "Designer", image: {img}}]} 
+            list={["React", "CSS", "Github", "Figma", "Node", "Javascript", "Responsive Design", "Validation Form", "Investigation", "Clean Code"]} 
+          />
         </Route>
         <Route path="/"> 
           <div className='avion'>
