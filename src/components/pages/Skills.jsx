3bb44b734d9b315675559a3bcb0adbe286cf2154
Michellefsd <promichfsd@gmail.com>
2023-06-04 18:52:23 -0300

    improve animation of aeroplane improve content in skills

--- src/components/pages/Skills.jsx
+++ src/components/pages/Skills.jsx
@@ -1,13 +1,22 @@
 import './Skills.css'
-import React from 'react';
+import { useState } from 'react';
 import MiniCard from '../MiniCard';
-import { FaGlobe, FaBookOpen, FaHandHoldingHeart, FaLightbulb } from 'react-icons/fa';
+import SkillsDetail from '../SkillsDetail';
+import { FaGlobe, FaBookOpen, FaHandHoldingHeart, FaLightbulb, FaFileCode } from 'react-icons/fa';
 import { DiHtml5, DiCss3, DiJsBadge, DiGithubBadge } from 'react-icons/di';
-import { SiPhp } from 'react-icons/si';
 import { BsFillDatabaseFill } from 'react-icons/bs';
 
 
 function Skills() {
+  const [ showDetailsSkills, setShowDetailsSkills ] = useState(0);
+
+  const showDetails = (number) => {
+    setShowDetailsSkills(number)
+  }
+  const hideDetails = () => {
+    setShowDetailsSkills(0)
+  }
+
   return (
     <div>
       <section id="skills" className='section'>
@@ -19,33 +28,40 @@ function Skills() {
               <MiniCard icon={<DiHtml5 />}>
                 HTML
               </MiniCard>
-              <MiniCard icon={<DiCss3 />}>
-                CSS
-              </MiniCard>
-              <MiniCard icon={<DiJsBadge />}>
+              <div onMouseOver={() => showDetails(2)} onMouseLeave={hideDetails}>
+                {(showDetailsSkills === 2) && <SkillsDetail list={["Sass", "Responsive Design", "Bootstrap", "Tailwind"]} />} 
+                <MiniCard icon={<DiCss3 />}>
+                  CSS
+                </MiniCard>
+              </div>
+              <MiniCard onMouseOver={() => showDetails(3)} onMouseLeave={hideDetails} icon={<DiJsBadge />}>
+              {(showDetailsSkills === 3) && <SkillsDetail list={["React", "Vue", "Jquery", "Node", "Typescript"]} />}
                 JavaScript
               </MiniCard>
-              <MiniCard icon={<DiGithubBadge />}>
+              <MiniCard onMouseOver={() => showDetails(4)} onMouseLeave={hideDetails} icon={<DiGithubBadge />}>
+              {(showDetailsSkills === 4) && <SkillsDetail list={["GitHub"]} />}
                 Git
               </MiniCard>
-              <MiniCard icon={<BsFillDatabaseFill />}>
+              <MiniCard onMouseOver={() => showDetails(5)} onMouseLeave={hideDetails} icon={<BsFillDatabaseFill />}>
+              {(showDetailsSkills === 5) && <SkillsDetail list={["MySQL"]} />}
                 Databases
               </MiniCard>
-              <MiniCard icon={<SiPhp />}>
-                Php
+              <MiniCard onMouseOver={() => showDetails(6)} onMouseLeave={hideDetails} icon={<FaFileCode />}>
+              {(showDetailsSkills === 6) && <SkillsDetail list={["Php Basics", "Node Basics", "Python Basics"]} />}
+                Backend
               </MiniCard>
             </div>
           </div>
           <div className='section--skills'>
             <h3>Soft Skills</h3>
             <div className='section--minicard'>
-              <MiniCard icon={<FaLightbulb />}>
+              <MiniCard onMouseOver={() => showDetails(20)} onMouseLeave={hideDetails} icon={<FaLightbulb />}>
                 Proactive
               </MiniCard>
               <MiniCard icon={<FaHandHoldingHeart />}>
                 Empathy
               </MiniCard>
-              <MiniCard icon={<FaBookOpen />}>
+              <MiniCard onMouseOver={() => showDetails(22)} onMouseLeave={hideDetails} icon={<FaBookOpen />}>
                 Self-Learning
               </MiniCard>
             </div>
@@ -53,13 +69,13 @@ function Skills() {
           <div className='section--skills'>
             <h3>Languages</h3>
             <div className='section--minicard'>
-              <MiniCard icon={<FaGlobe />}>
+              <MiniCard onMouseOver={() => showDetails(10)} onMouseLeave={hideDetails} icon={<FaGlobe />}>
                 English
               </MiniCard>
-              <MiniCard icon={<FaGlobe />}>
+              <MiniCard onMouseOver={() => showDetails(11)} onMouseLeave={hideDetails} icon={<FaGlobe />}>
                 Portuguese
               </MiniCard>
-              <MiniCard icon={<FaGlobe />}>
+              <MiniCard onMouseOver={() => showDetails(12)} onMouseLeave={hideDetails} icon={<FaGlobe />}>
                 Spanish
               </MiniCard>
             </div>
